4d48f462742901f50d794768ae6c932565ef3ed7
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2023-07-29 22:30:12 -0500

    [chore] Add Admin header text

--- tours/urls.py
+++ tours/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = "CDMX Tours Admin"
+admin.site.site_title = "CDMX Tours Admin Portal"
+admin.site.index_title = "Welcome to CDMX Tours Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
